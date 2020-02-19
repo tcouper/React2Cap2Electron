b64f4978b98c5febb3180a119e5cdd8b2714ee19
Tim Couper <tim.couper@signaturelnd.com>
2020-02-19 12:33:47 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import isElectron from 'is-electron';
+
+
+function handleClick (e) {
+  
+  if (isElectron()) {
+    console.log("Detected Electron:  Using IPC")
+    const {ipcRenderer} = window.require('electron')
+    ipcRenderer.send("FROM_WINDOW", "Hi")
+
+  } else {
+
+    console.log(" Not Supported - requires the Desktop app")
+
+  }
+
+}
+
+
+if (isElectron()) {
+
+  const {ipcRenderer} = window.require('electron')
+
+  ipcRenderer.on("FROM_IPC", (data) => {
+    console.log(`Received ${data} from main process`);
+  });
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>React to Capacitor to Electron</h1>
+      <p>This is a basic test harness to demonstrate accessing Electron APIs from within REACT</p>
+
+      <button onClick={handleClick}>Click Me</button>
+
+
+
     </div>
   );
 }
